a2382b0752fcc0b7fadd8848c47213851d8b358c
leticiacarvalho04 <leticiahelena.oliveira@outlook.com>
2024-02-23 13:55:27 -0300

    Lista I finalizada

---
+++ teste/index.ts
@@ -0,0 +1,45 @@
+import Cliente from "../modelos/cliente";
+import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
+
+let cliente = new Cliente()
+cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
+cliente.nomeSocial = `Dom Pedro II`
+cliente.dataCadastro = new Date(1840, 6, 23)
+cliente.dataNascimento = new Date(1825, 11, 2)
+
+let endereco = new Endereco()
+endereco.rua = `R. do Catete`
+endereco.bairro = `Copacabana`
+endereco.cidade = `Rio de Janeiro`
+endereco.estado = `Rio de Janeiro`
+endereco.pais = `Brasil`
+endereco.codigoPostal = `22220-000`
+cliente.endereco = endereco
+
+let telefone = new Telefone()
+telefone.ddd = `12`
+telefone.numero = `421147754`
+cliente.telefones.push(telefone)
+
+let telefone1 = new Telefone()
+telefone1.ddd = `13`
+telefone1.numero = `384623613`
+cliente.telefones.push(telefone1)
+
+let dependente = new Cliente()
+dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
+dependente.nomeSocial = `Princesa Isabel`
+dependente.dataCadastro = new Date(1921, 10, 14)
+dependente.dataNascimento = new Date(1846, 6, 29)
+dependente.endereco = (cliente.endereco.clonar() as Endereco)
+dependente.telefones = (cliente.telefones.map(telefone => telefone.clonar()) as Telefone[])
+dependente.titular = cliente
+cliente.dependentes.push(dependente)
+
+console.log(`\nCLIENTE`);
+console.log(`----------------`);
+console.log(cliente);
+console.log(`\nDEPENDENTE`);
+console.log(`----------------`);
+console.log(dependente);
